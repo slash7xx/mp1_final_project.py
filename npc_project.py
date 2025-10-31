6481ebc45b2958dbf6aee11433d1a0c2927cc5e8
slash7xx <li20291684@student.nhvweb.net>
2025-10-31 18:23:27 +0000

    project

--- npc_project.py
+++ npc_project.py
@@ -15,7 +15,8 @@ names = [
 final_phrase = ["Feel free to look around the village.", "Good luck on the rest of your journey.", "Oh. And in case I don't see you again, good morning, good afternoon, and good night.", "I'm.... I'm.... I'm...........................SPONGEBOBBBBBBBBBB","ARSON CAN BENCH PRESS 2 BLACK HOLESSSSS", "I'm Santa's elf!", "A mere monkey can never defeat a lion.", "This guy- this guy has a dang hidden sparky. What a greasy bum."]
 lvl = (range(1,100))
 job = ["Farmer", "Blacksmith", "Merchant", "Guard", "Healer", "Hunter", "Bard", "Alchemist"]
-height = (range(1,6.5).round(2))
+height = (range(1,7))
+monies = (range(1,100))
 
 gng = input("Generate NPC? y/n:")
 if gng == "y" or "Y":
@@ -23,12 +24,322 @@ if gng == "y" or "Y":
     npc_trait = random.choice(traits)
     npc_level = random.choice(lvl)
     npc_job = random.choice(job)
-    print(f"Name: {npc_name}\nTrait: {npc_trait}\nLevel: {npc_level}\nJob: {npc_job}")
+    npc_monies = random.choice(monies)
+    npc_height = random.choice(height)
+    if npc_monies <=15:
+        npc_stat = "brokie"
+    if npc_monies >15 and npc_monies <50:
+        npc_stat = "average Joe"
+    if npc_monies >=50:
+        npc_stat = "A sweat at life and people tell me to get a life bro."
+    
+    print(f"Name: {npc_name}\nTrait: {npc_trait}\nLevel: {npc_level}\nJob: {npc_job}\nWorth: {npc_monies}")
 else:
     print("Wow. At least test ur code man.")
 
 if gng == "y" or "Y":
     talk = input("Talk to NPC? y/n:")
     if talk == "y" or "Y":
-        print(f"---------------{npc_name}---------------")
-        print(f"Hi I'm {npc_name}. I'm lvl {npc_level} and my job is {npc_job}. Apparently, people know me for being {npc_trait}. I am {random.choice(height)} and {random.choice(final_phrase)}")
+        print(f"\t\t\t\t---------------{npc_name}---------------")
+        print(f"Hi I'm {npc_name}. I'm lvl {npc_level} and my job is {npc_job}. Apparently, people know me for being {npc_trait}. I am {npc_height} ft tall, I'm also a {npc_stat} and {random.choice(final_phrase)}")
+        if talk == "n" or "N":
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
